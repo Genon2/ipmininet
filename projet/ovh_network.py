f8e293205af2be1003319b12384e4b3a3670b879
Genon2 <francois.genon@gmail.com>
2020-10-21 22:10:11 +0200

    Added eBGP but not exchanging packets

--- projet/ovh_network.py
+++ projet/ovh_network.py
@@ -11,12 +11,18 @@ class MyTopology(IPTopo):
    
     def build(self, *args, **kwargs):
         
-        # Building stubs
-        chi_1_charter = self.addHost("charter1")
-        ash_1_charter = self.addHost("charter2")
-        ash_1_amazon = self.addHost("amazon1")
-        ash_5_amazon = self.addHost("amazon2")
+        # Adding stubs to AS2 and building AS2 routers
+        as2_chi_1_charter = self.addHost("charter1")
+        as2_ash_1_charter = self.addHost("charter2")
+        as2_ash_1_amazon = self.addHost("amazon1")
+        as2_ash_5_amazon = self.addHost("amazon2")
+        # Two routers needed to build an AS
+        as2_r1 = self.addRouter("as2_r1")
+        as2_r2 = self.addRouter("as2_r2")
+        
 
+        # Building OVH Network
+        as1_host = self.addHost("as1_host") # test routers used for pingall when checking eBGP
         # Building New York routers
         nwk_1, nwk_5  = self.addRouters("nwk_1", "nwk_5")
         # Building Bhs routers
@@ -30,14 +36,23 @@ class MyTopology(IPTopo):
         # Building asia's routers abstractly
         asia = self.addRouter("asia")
 
+        # Adding OVH AS
         as16276_routers = [nwk_1,nwk_5,bhs_g1,bhs_g2,chi_1,chi_5,ash_1,ash_5,europe,asia]
         
-        self.addAS(16276, (nwk_1,nwk_5,bhs_g1,bhs_g2,chi_1,chi_5,ash_1,ash_5,europe,asia))
-        
-        # Adding OSPFv3 and BGP to all routers
+        # Adding OSPFv3 and BGP to OVH Network
         for r in as16276_routers:
             r.addDaemon(OSPF6)
             r.addDaemon(BGP)
+
+        self.addAS(16276, (nwk_1,nwk_5,bhs_g1,bhs_g2,chi_1,chi_5,ash_1,ash_5,europe,asia))
+
+        # Adding AS2 Daemons
+        as2_r1.addDaemon(OSPF6)
+        as2_r1.addDaemon(BGP)
+        as2_r2.addDaemon(OSPF6)
+        as2_r2.addDaemon(BGP)
+        self.addAS(2, (as2_r1, as2_r2))
+        
 
         # Adding Links  and igp_costs between all routers
         self.addLink(nwk_1,  nwk_5,  igp_cost=1)
@@ -66,14 +81,26 @@ class MyTopology(IPTopo):
         self.addLink(asia,   ash_1,  igp_cost=50)
         self.addLink(asia,   ash_5,  igp_cost=50)
         
-        # Connecting stubs
-        self.addLink(chi_1_charter, chi_1)
-        self.addLink(ash_1, ash_1_charter)
-        self.addLink(ash_1, ash_1_amazon)
-        self.addLink(ash_5, ash_5_amazon)
+        # Linking router AS2 to OVH routers
+        self.addLink(chi_1, as2_r1)
+        self.addLink(ash_1, as2_r1)
+        self.addLink(ash_5, as2_r1)
+        # Linking AS2 stubs to as2 routers
+        self.addLink(as2_r1, as2_r2)
+        self.addLink(as2_ash_1_amazon, as2_r1)
+        self.addLink(as2_chi_1_charter,as2_r1)
+        self.addLink(as2_ash_5_amazon, as2_r1)
+        self.addLink(as2_ash_1_charter, as2_r1)
+        self.addLink(as1_host, nwk_1)
 
+        # Setting up OVH Network Route Reflectors
         set_rr(self, rr=nwk_1, peers=[nwk_5,bhs_g1,bhs_g2,chi_1,chi_5,ash_1,ash_5,europe,asia])
         set_rr(self, rr=chi_1, peers=[nwk_1,nwk_5,bhs_g1,bhs_g2,chi_5,ash_1,ash_5,europe,asia])
+
+        # Adding eBGP sessions between AS2 and OVH Network
+        ebgp_session(self, as2_r1, chi_1)
+        ebgp_session(self, as2_r1,ash_1)
+        ebgp_session(self, as2_r1,ash_5)
 
         super().build(*args, **kwargs)
 
